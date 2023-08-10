6b526ca2f7f864687d3ee1fc1d90b41453d62446
Grégory Bataille <gregory.bataille@gmail.com>
2023-08-10 16:55:20 +0200

    better test shortcut

--- .config/nvim/lua/plug_nvim-test.lua
+++ .config/nvim/lua/plug_nvim-test.lua
@@ -1,18 +1,18 @@
 require('nvim-test').setup {
-  run = true, -- run tests (using for debug)
-  commands_create = true, -- create commands (TestFile, TestLast, ...)
-  filename_modifier = ":.", -- modify filenames before tests run(:h filename-modifiers)
-  silent = false, -- less notifications
-  term = "terminal", -- a terminal to run ("terminal"|"toggleterm")
+  run = true,                 -- run tests (using for debug)
+  commands_create = true,     -- create commands (TestFile, TestLast, ...)
+  filename_modifier = ":.",   -- modify filenames before tests run(:h filename-modifiers)
+  silent = false,             -- less notifications
+  term = "terminal",          -- a terminal to run ("terminal"|"toggleterm")
   termOpts = {
     direction = "horizontal", -- terminal's direction ("horizontal"|"vertical"|"float")
-    width = 96, -- terminal's width (for vertical|float)
-    height = 30, -- terminal's height (for horizontal|float)
-    go_back = false, -- return focus to original window after executing
-    stopinsert = false, -- exit from insert mode (true|false|"auto")
-    keep_one = true, -- keep only one terminal for testing
+    width = 96,               -- terminal's width (for vertical|float)
+    height = 30,              -- terminal's height (for horizontal|float)
+    go_back = false,          -- return focus to original window after executing
+    stopinsert = false,       -- exit from insert mode (true|false|"auto")
+    keep_one = true,          -- keep only one terminal for testing
   },
-  runners = { -- setup tests runners
+  runners = {                 -- setup tests runners
     cs = "nvim-test.runners.dotnet",
     go = "nvim-test.runners.go-test",
     haskell = "nvim-test.runners.hspec",
@@ -38,54 +38,70 @@ local defaultGoTest = function()
     command = "grc",
     args = { "go", "test", "-v" },
     file_pattern = "\\v([^.]+_test)\\.go$", -- determine whether a file is a testfile
-    find_files = { "{name}_test.go" }, -- find testfile for a file
+    find_files = { "{name}_test.go" },      -- find testfile for a file
   }
 end
 
 -- See mappings.lua
-RunSQLServerTest = function()
+local runWithTagsAndEnv = function(tags, env)
   local test = require("nvim-test")
 
   require('nvim-test.runners.go-test'):setup {
     command = "grc",
-    args = { "go", "test", "-v", "-tags", "integration,acceptance,manual", "-count", "1" },
+    args = { "go", "test", "-v", "-tags", tags, "-count", "1" },
     file_pattern = "\\v([^.]+_test)\\.go$", -- determine whether a file is a testfile
-    find_files = { "{name}_test.go" }, -- find testfile for a file
-    env = {
-      TG_DB_HOST = "localhost",
-      TG_DB_PORT = 1433,
-      TG_DB_USER = "sa",
-      TG_DB_PASSWORD = "MyPass@word0",
-      TG_DB_DRIVER = "sqlserver",
-    },
+    find_files = { "{name}_test.go" },      -- find testfile for a file
+    env = env,
   }
 
   test.run('nearest')
 
   defaultGoTest()
+end
+
+RunSQLServerTest = function()
+  runWithTagsAndEnv("integration,acceptance", {
+    TG_DB_HOST = "localhost",
+    TG_DB_PORT = 1433,
+    TG_DB_USER = "sa",
+    TG_DB_PASSWORD = "MyPass@word0",
+    TG_DB_DRIVER = "sqlserver",
+  }
+  )
+end
+
+RunSQLServerManualTest = function()
+  runWithTagsAndEnv("manual", {
+    TG_DB_HOST = "localhost",
+    TG_DB_PORT = 1433,
+    TG_DB_USER = "sa",
+    TG_DB_PASSWORD = "MyPass@word0",
+    TG_DB_DRIVER = "sqlserver",
+  }
+  )
 end
 
 -- See mappings.lua
 RunCrdbTest = function()
-  local test = require("nvim-test")
-
-  require('nvim-test.runners.go-test'):setup {
-    command = "grc",
-    args = { "go", "test", "-v", "-tags", "integration,acceptance,manual", "-count", "1" },
-    file_pattern = "\\v([^.]+_test)\\.go$", -- determine whether a file is a testfile
-    find_files = { "{name}_test.go" }, -- find testfile for a file
-    env = {
-      TG_DB_HOST = "localhost",
-      TG_DB_PORT = 26257,
-      TG_DB_USER = "root",
-      TG_DB_PASSWORD = "root",
-      TG_DB_DRIVER = "cockroach",
-    },
+  runWithTagsAndEnv("integration,acceptance", {
+    TG_DB_HOST = "localhost",
+    TG_DB_PORT = 26257,
+    TG_DB_USER = "root",
+    TG_DB_PASSWORD = "root",
+    TG_DB_DRIVER = "cockroach",
   }
+  )
+end
 
-  test.run('nearest')
-
-  defaultGoTest()
+RunCrdbManualTest = function()
+  runWithTagsAndEnv("manual", {
+    TG_DB_HOST = "localhost",
+    TG_DB_PORT = 26257,
+    TG_DB_USER = "root",
+    TG_DB_PASSWORD = "root",
+    TG_DB_DRIVER = "cockroach",
+  }
+  )
 end
 
 defaultGoTest() -- init go test with grc for colors
