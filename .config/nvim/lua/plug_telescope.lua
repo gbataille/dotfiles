bf06d72042b8c64258e47d3cf4c314a590df04fd
Grégory Bataille <gregory.bataille@gmail.com>
2023-08-10 06:47:07 +0200

    telescope to qflist

--- .config/nvim/lua/plug_telescope.lua
+++ .config/nvim/lua/plug_telescope.lua
@@ -1,6 +1,6 @@
-require('telescope').setup{
+require('telescope').setup {
   defaults = {
-    file_ignore_patterns = {".pb.go", ".pb.gw.go", "internal/api/clients/", "/generated/"},
+    file_ignore_patterns = { ".pb.go", ".pb.gw.go", "internal/api/clients/", "/generated/" },
   }
 }
 
@@ -10,3 +10,5 @@ local actions = require "telescope.actions"
 -- Allow navigation with typical j/k
 mappings.default_mappings["i"]["<C-j>"] = actions.move_selection_next
 mappings.default_mappings["i"]["<C-k>"] = actions.move_selection_previous
+
+mappings.default_mappings["n"]["q"] = actions.send_to_qflist + actions.open_qflist
