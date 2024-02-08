9893646d4fcafc4f0948e900180607b58e9d9407
Grégory Bataille <gregory.bataille@gmail.com>
2024-02-08 09:54:39 +0100

    multi

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -75,17 +75,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end
 })
-vim.api.nvim_create_autocmd("LspAttach", {
-  pattern = "*.java",
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    -- ensure that only the jdtls client is activated
-    if client.name == "jdtls" then
-      require("jdtls").setup_dap()
-      require("jdtls.dap").setup_dap_main_class_configs()
-    end
-  end,
-})
+-- Disable Java
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   pattern = "*.java",
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     -- ensure that only the jdtls client is activated
+--     if client.name == "jdtls" then
+--       require("jdtls").setup_dap()
+--       require("jdtls.dap").setup_dap_main_class_configs()
+--     end
+--   end,
+-- })
 
 local nvim_lsp = require('lspconfig')
 
@@ -265,151 +266,151 @@ require 'lspconfig'.kotlin_language_server.setup {
 -- Seems to attach even on non-java... to check
 -- ############## JAVA #################
 
-local plugin = 'jdtls'
-local status_ok, jdtls = pcall(require, plugin)
-if not status_ok then
-  print("ERROR: " .. plugin .. " plugin")
-  return
-end
-
-
-local java_home = '/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home'
-
-local data_path = vim.fn.stdpath("data")
-local jdtls_path = data_path .. '/mason/packages/jdtls'
-local platform_config = jdtls_path .. "/config_mac"
-local path_to_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
-local lombok_path = jdtls_path .. "/lombok.jar"
-
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = jdtls.setup.find_root(root_markers)
-if root_dir == "" then
-  return
-end
-
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = data_path .. '/site/java/workspace-root/' .. project_name
-os.execute("mkdir -p " .. workspace_dir)
-
--- java-debug and vscode-java-test
--- see https://github.com/mfussenegger/nvim-jdtls?tab=readme-ov-file
-local bundles = {
-  vim.fn.glob(
-    "/Users/gbataille/Documents/JavaVimDeps/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
-    1),
-};
-vim.list_extend(bundles,
-  vim.split(vim.fn.glob("/Users/gbataille/Documents/JavaVimDeps/vscode-java-test/server/*.jar", 1), "\n"))
-
--- Main Config
-local config = {
-  -- The command that starts the language server
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  cmd = {
-    java_home .. '/bin/java',
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '-javaagent:' .. lombok_path,
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', path_to_jar,
-    '-configuration', platform_config,
-    '-data', workspace_dir,
-  },
-
-  -- This is the default if not provided, you can remove it. Or adjust as needed.
-  -- One dedicated LSP server & client will be started per unique root_dir
-  -- root_dir = root_dir,
-
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
-  settings = {
-    java = {
-      home = java_home,
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      eclipse = {
-        downloadSources = true,
-      },
-      configuration = {
-        updateBuildConfiguration = "interactive",
-        -- runtimes = {
-        -- 	{
-        -- 		name = "JDK-21",
-        -- 		path = "/usr/local/opt/openjdk"
-        -- 	}
-        -- }
-      },
-      maven = {
-        downloadSources = true,
-      },
-      implementationsCodeLens = {
-        enabled = true,
-      },
-      referencesCodeLens = {
-        enabled = true,
-      },
-      references = {
-        includeDecompiledSources = true,
-      },
-      format = {
-        enabled = true,
-        settings = {
-          url = jdtls_path .. "/intellij-java-google-style.xml",
-          profile = "GoogleStyle",
-        },
-      },
-
-    },
-    signatureHelp = { enabled = true },
-    completion = {
-      favoriteStaticMembers = {
-        "org.hamcrest.MatcherAssert.assertThat",
-        "org.hamcrest.Matchers.*",
-        "org.hamcrest.CoreMatchers.*",
-        "org.junit.jupiter.api.Assertions.*",
-        "java.util.Objects.requireNonNull",
-        "java.util.Objects.requireNonNullElse",
-        "org.mockito.Mockito.*",
-      },
-      importOrder = {
-        "java",
-        "javax",
-        "com",
-        "org"
-      },
-    },
-    extendedClientCapabilities = extendedClientCapabilities,
-    sources = {
-      organizeImports = {
-        starThreshold = 9999,
-        staticStarThreshold = 9999,
-      },
-    },
-    codeGeneration = {
-      toString = {
-        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-      },
-      useBlocks = true,
-    },
-  },
-
-  flags = {
-    allow_incremental_sync = true,
-  },
-  init_options = {
-    bundles = bundles
-  },
-  on_attach = on_attach,
-}
-
--- require('jdtls').start_or_attach(config)
-
-nvim_lsp.jdtls.setup(config)
-
---vim.lsp.set_log_level("debug")
+-- local plugin = 'jdtls'
+-- local status_ok, jdtls = pcall(require, plugin)
+-- if not status_ok then
+--   print("ERROR: " .. plugin .. " plugin")
+--   return
+-- end
+--
+--
+-- local java_home = '/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home'
+--
+-- local data_path = vim.fn.stdpath("data")
+-- local jdtls_path = data_path .. '/mason/packages/jdtls'
+-- local platform_config = jdtls_path .. "/config_mac"
+-- local path_to_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+-- local lombok_path = jdtls_path .. "/lombok.jar"
+--
+-- local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+-- local root_dir = jdtls.setup.find_root(root_markers)
+-- if root_dir == "" then
+--   return
+-- end
+--
+-- local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+-- local workspace_dir = data_path .. '/site/java/workspace-root/' .. project_name
+-- os.execute("mkdir -p " .. workspace_dir)
+--
+-- -- java-debug and vscode-java-test
+-- -- see https://github.com/mfussenegger/nvim-jdtls?tab=readme-ov-file
+-- local bundles = {
+--   vim.fn.glob(
+--     "/Users/gbataille/Documents/JavaVimDeps/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+--     1),
+-- };
+-- vim.list_extend(bundles,
+--   vim.split(vim.fn.glob("/Users/gbataille/Documents/JavaVimDeps/vscode-java-test/server/*.jar", 1), "\n"))
+--
+-- -- Main Config
+-- local config = {
+--   -- The command that starts the language server
+--   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+--   cmd = {
+--     java_home .. '/bin/java',
+--     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+--     '-Dosgi.bundles.defaultStartLevel=4',
+--     '-Declipse.product=org.eclipse.jdt.ls.core.product',
+--     '-Dlog.protocol=true',
+--     '-Dlog.level=ALL',
+--     '-Xms1g',
+--     '-javaagent:' .. lombok_path,
+--     '--add-modules=ALL-SYSTEM',
+--     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+--     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+--     '-jar', path_to_jar,
+--     '-configuration', platform_config,
+--     '-data', workspace_dir,
+--   },
+--
+--   -- This is the default if not provided, you can remove it. Or adjust as needed.
+--   -- One dedicated LSP server & client will be started per unique root_dir
+--   -- root_dir = root_dir,
+--
+--   -- Here you can configure eclipse.jdt.ls specific settings
+--   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+--   -- for a list of options
+--   settings = {
+--     java = {
+--       home = java_home,
+--       '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+--       eclipse = {
+--         downloadSources = true,
+--       },
+--       configuration = {
+--         updateBuildConfiguration = "interactive",
+--         -- runtimes = {
+--         -- 	{
+--         -- 		name = "JDK-21",
+--         -- 		path = "/usr/local/opt/openjdk"
+--         -- 	}
+--         -- }
+--       },
+--       maven = {
+--         downloadSources = true,
+--       },
+--       implementationsCodeLens = {
+--         enabled = true,
+--       },
+--       referencesCodeLens = {
+--         enabled = true,
+--       },
+--       references = {
+--         includeDecompiledSources = true,
+--       },
+--       format = {
+--         enabled = true,
+--         settings = {
+--           url = jdtls_path .. "/intellij-java-google-style.xml",
+--           profile = "GoogleStyle",
+--         },
+--       },
+--
+--     },
+--     signatureHelp = { enabled = true },
+--     completion = {
+--       favoriteStaticMembers = {
+--         "org.hamcrest.MatcherAssert.assertThat",
+--         "org.hamcrest.Matchers.*",
+--         "org.hamcrest.CoreMatchers.*",
+--         "org.junit.jupiter.api.Assertions.*",
+--         "java.util.Objects.requireNonNull",
+--         "java.util.Objects.requireNonNullElse",
+--         "org.mockito.Mockito.*",
+--       },
+--       importOrder = {
+--         "java",
+--         "javax",
+--         "com",
+--         "org"
+--       },
+--     },
+--     extendedClientCapabilities = extendedClientCapabilities,
+--     sources = {
+--       organizeImports = {
+--         starThreshold = 9999,
+--         staticStarThreshold = 9999,
+--       },
+--     },
+--     codeGeneration = {
+--       toString = {
+--         template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+--       },
+--       useBlocks = true,
+--     },
+--   },
+--
+--   flags = {
+--     allow_incremental_sync = true,
+--   },
+--   init_options = {
+--     bundles = bundles
+--   },
+--   on_attach = on_attach,
+-- }
+--
+-- -- require('jdtls').start_or_attach(config)
+--
+-- nvim_lsp.jdtls.setup(config)
+--
+-- --vim.lsp.set_log_level("debug")
