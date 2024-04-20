7954c48fbc5d7cf4f4e11198d38c10c6cfe25eb8
Grégory Bataille <gregory.bataille@gmail.com>
2024-04-20 16:17:32 +0200

    try to add more nix setup

---
+++ nix-darwin-config/aliases.nix
@@ -0,0 +1,3 @@
+{
+  cdp = "cd ~/Documents/Prog/";
+}
--- nix-darwin-config/flake.nix
+++ nix-darwin-config/flake.nix
@@ -10,10 +10,15 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     pkgListFn = import ./systemPkgs.nix;
+    aliases = import ./aliases.nix;
+
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = pkgListFn { inherit pkgs; } ;
+      environment.shellAliases = {
+        cdp = "cd ~/Documents/Prog/";
+      };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
@@ -23,28 +28,71 @@
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        enableFzfCompletion = true;
+        enableFzfHistory = true;
+        enableSyntaxHighlighting = true;
+      };
       # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+      programs.direnv = {
+        enable = true;
+      };
 
       # Sudo with touchId
       security.pam.enableSudoTouchIdAuth = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
 
-      system.defaults = {
-        dock.autohide = true;
-        dock.mru-spaces = false;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        screensaver.askForPasswordDelay = 5;
+      system = {
+        # Set Git commit hash for darwin-version.
+        configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        stateVersion = 4;
+
+        defaults = {
+          finder = {
+            AppleShowAllExtensions = true;
+            FXPreferredViewStyle = "clmv";
+            ShowPathbar = true;
+          };
+
+          screensaver = {
+            askForPasswordDelay = 5;
+          };
+
+          NSGlobalDomain = {
+            AppleShowAllFiles = true;
+            AppleKeyboardUIMode = 3;
+            InitialKeyRepeat = 500;
+            NSNavPanelExpandedStateForSaveMode = true;
+            NSNavPanelExpandedStateForSaveMode2 = true;
+            "com.apple.keyboard.fnState" = true;
+            "com.apple.mouse.tapBehavior" = 1;
+          };
+
+          dock = {
+            appswitcher-all-displays = true;
+            autohide = true;
+            mru-spaces = false;
+          };
+
+          trackpad = {
+            Clicking = true;
+            TrackpadRightClick = true;
+            TrackpadThreeFingerDrag = true;
+          };
+
+        };
+        keyboard = {
+          enableKeyMapping = true;
+          remapCapsLockToControl = true;
+        };
       };
     };
   in
