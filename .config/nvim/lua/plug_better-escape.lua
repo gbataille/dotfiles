057bd9a32aa62410bdf1cd17e2b8456f81be4d9c
Grégory Bataille <gregory.bataille@gmail.com>
2024-07-22 07:22:28 +0200

    nvim plugin config changes

--- .config/nvim/lua/plug_better-escape.lua
+++ .config/nvim/lua/plug_better-escape.lua
@@ -1,6 +1,42 @@
+esc_and_clean_empty = function()
+    vim.api.nvim_input("<esc>")
+    local current_line = vim.api.nvim_get_current_line()
+    if current_line:match("^%s+j$") then
+        vim.api.nvim_set_current_line("")
+    end
+end
+
 require("better_escape").setup {
-    mapping = { "jj" },         -- a table with mappings to use
     timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    clear_empty_lines = true,   -- clear line after escaping if there is only whitespace
-    keys = "<Esc>",             -- keys used for escaping, if it is a function will use the result everytime
+    mappings = {
+        i = {
+            j = {
+                -- These can all also be functions
+                k = esc_and_clean_empty,
+                j = esc_and_clean_empty,
+            },
+        },
+        c = {
+            j = {
+                k = esc_and_clean_empty,
+                j = esc_and_clean_empty,
+            },
+        },
+        t = {
+            j = {
+                k = esc_and_clean_empty,
+                j = esc_and_clean_empty,
+            },
+        },
+        v = {
+            j = {
+                k = esc_and_clean_empty,
+            },
+        },
+        s = {
+            j = {
+                k = esc_and_clean_empty,
+            },
+        },
+    },
 }
--- .config/nvim/lua/plug_gitsigns.lua
+++ .config/nvim/lua/plug_gitsigns.lua
@@ -1,21 +1,30 @@
 require('gitsigns').setup {
   signs = {
-    add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-    change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-    delete       = { hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-    untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
-    interval = 1000,
     follow_files = true
   },
-  attach_to_untracked = true,
+  auto_attach = true,
+  attach_to_untracked = false,
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
@@ -35,8 +44,5 @@ require('gitsigns').setup {
     relative = 'cursor',
     row = 0,
     col = 1
-  },
-  yadm = {
-    enable = false
   },
 }
